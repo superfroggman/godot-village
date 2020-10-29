2bba99659bd6f5c5696bce3d6cdb9c839c981f99
superfroggman <anrehelenius@gmail.com>
2020-10-29 15:24:03 +0100

    began working on movement

---
+++ godot-village/Scripts/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+var speed = 2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	velocity*=speed
+	
+	move_and_collide(velocity, true, true, false)
+	
+	position += velocity * delta
