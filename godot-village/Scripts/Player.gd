46d2e0249f771685f4468e552a25ca9bcd4cc084
superfroggman <anrehelenius@gmail.com>
2020-10-29 16:24:15 +0100

    added analog joystick support

--- godot-village/Scripts/Player.gd
+++ godot-village/Scripts/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
-var speed = 2
+export var speed = 5
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +14,15 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2()
 	
+	var deadZone = 0.2
+	if Input.get_connected_joypads().size() > 0:
+		var xAxis = Input.get_joy_axis(0,JOY_AXIS_0)
+		var yAxis = Input.get_joy_axis(0,JOY_AXIS_1)
+		if abs(xAxis) > deadZone:
+			velocity.x = xAxis
+		if abs(yAxis) > deadZone:
+			velocity.y = yAxis
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -22,8 +31,13 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		
+	velocity.x = clamp(velocity.x, -1, 1)
+	velocity.y = clamp(velocity.y, -1, 1)
+	
+	#if velocity.length() > 0:
+	#	velocity = velocity.normalized() * speed
+		
 	velocity*=speed
 	
 	move_and_collide(velocity, true, true, false)
