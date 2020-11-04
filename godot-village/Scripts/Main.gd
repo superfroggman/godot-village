0e9fb6bbd0ccb6d5fd8b21d96e0bec82872a3fe2
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-11-04 11:55:58 +0100

    added choosing between blocks

--- godot-village/Scripts/Main.gd
+++ godot-village/Scripts/Main.gd
@@ -5,44 +5,51 @@ extends Node2D
 export var gridSize = 64
 export (PackedScene) var Floor
 export (PackedScene) var Wall
+export(Array, PackedScene) var Blocks
 
 
 var width = 100
 var height = 100
 var matrix = []
 
+var currentBlockType = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in range(width):
-		matrix.append([])
-		for y in range(height):
-			matrix[x].append(0)
-
-			
-	pass # Replace with function body.
-
+		var col = []
+		col.resize(height)
+		matrix.append(col)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if(Input.is_action_pressed("block1")):
+		currentBlockType = 0
+	if(Input.is_action_pressed("block2")):
+		currentBlockType = 1
+	
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
-		
-		var gridPos = Vector2()
-		gridPos.x = round(($Player.position.x + event.position.x - get_viewport_rect().size.x/2)/gridSize)
-		gridPos.y = round(($Player.position.y + event.position.y - get_viewport_rect().size.y/2)/gridSize)
-		
-		if(event.is_action_pressed("mouse_left")):
-			if(!matrix[gridPos.x][gridPos.y]):
-				var myFloor = Floor.instance()
-				myFloor.position = gridPos*gridSize
-				matrix[gridPos.x][gridPos.y] = myFloor
-				add_child(myFloor)
-		elif(event.is_action_pressed("mouse_right")):
-			if(matrix[gridPos.x][gridPos.y]):
-				matrix[gridPos.x][gridPos.y].queue_free()
+		placeBlocks(event)
+
+
+func placeBlocks(event):
+	var gridPos = Vector2()
+	gridPos.x = round(($Player.position.x + event.position.x - get_viewport_rect().size.x/2)/gridSize)
+	gridPos.y = round(($Player.position.y + event.position.y - get_viewport_rect().size.y/2)/gridSize)
+	
+	if(event.is_action_pressed("mouse_left") && gridPos.x >= 0 && gridPos.y >= 0):
+		if(!matrix[gridPos.x][gridPos.y]):
+			print("x: ", gridPos.x, " y: ", gridPos.y, " cool: ", matrix[gridPos.x][gridPos.y])
+			var block = Blocks[currentBlockType].instance()
+			block.position = gridPos*gridSize
+			matrix[gridPos.x][gridPos.y] = block
+			add_child(block)
+	elif(event.is_action_pressed("mouse_right")):
+		if(matrix[gridPos.x][gridPos.y]):
+			matrix[gridPos.x][gridPos.y].queue_free()
 
 
 
