980f54c30d27a451d934676e0853e3bb072458b4
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-11-06 13:11:04 +0100

    ok lets just revert

--- godot-village/Scripts/Plot.gd
+++ godot-village/Scripts/Plot.gd
@@ -2,21 +2,27 @@ extends Area2D
 
 export var gridSize = 64
 export(Array, PackedScene) var blocks
-export(PackedScene) var blockContainer
+var node = preload("res://Scenes/Node.tscn")
+var blockContainer = preload("res://Scenes/BlockContainer.tscn")
 
 var width = 8
 var height = 8
-
-var placedBlocks = []
 
 var currentBlockType = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in range(width):
-		placedBlocks.append([])
+		#placedBlocks.append([])
+		var column = node.instance()
+		column.name = String(x)
+		add_child(column)
 		for y in range(height):
-			placedBlocks[x].append(blockContainer)
+			var block = blockContainer.instance()
+			block.name = String(y)
+			get_node(String(x)).add_child(block)
+			
+			print("x: ", get_node(String(x)).name, " y: ", get_node(String(x)).get_node(String(y)).name)
 
 
 func _on_Plot_input_event(viewport, event, shape_idx):
@@ -33,12 +39,17 @@ func _on_Plot_input_event(viewport, event, shape_idx):
 			var block = blocks[currentBlockType].instance()
 			block.position = gridPos*gridSize+Vector2(32,32)
 			
-			print(placedBlocks[gridPos.x][gridPos.y].get_node("Roof"))
+			if(get_node(String(gridPos.x)) && get_node(String(gridPos.x)).get_node(String(gridPos.y))):
+				var container = get_node(String(gridPos.x)).get_node(String(gridPos.y))
+				container.add_child(block)
+			#print(placedBlocks[gridPos.x][gridPos.y].get_children())
+			#print(placedBlocks[gridPos.x][gridPos.y].get_node("Roof"))
 			
 
 			
 		elif(event.is_action_pressed("mouse_right")):
-			placedBlocks = removeBlock(placedBlocks, gridPos)
+			#placedBlocks = removeBlock(placedBlocks, gridPos)
+			pass
 
 
 
@@ -58,21 +69,21 @@ func _process(delta):
 
 
 
-
-func _on_Plot_body_entered(body):
-	for x in placedBlocks:
-		for y in x:
-			var roof = y.get_node("Roof")
-			y.getnode
-			
-			if(roof):
-				var animPlayer = roof.get_node("AnimationPlayer")
-				if(animPlayer):
-					animPlayer.current_animation = "FadeOut"
-
-func _on_Plot_body_exited(body):
-	for x in placedBlocks:
-		for y in x:
-			var animPlayer = y.get_node("Roof").get_node("AnimationPlayer")
-			if(animPlayer):
-				animPlayer.current_animation = "FadeOut"
+#
+#func _on_Plot_body_entered(body):
+#	for x in placedBlocks:
+#		for y in x:
+#			var roof = y.get_node("Roof")
+#
+#
+#			if(roof):
+#				var animPlayer = roof.get_node("AnimationPlayer")
+#				if(animPlayer):
+#					animPlayer.current_animation = "FadeOut"
+#
+#func _on_Plot_body_exited(body):
+#	for x in placedBlocks:
+#		for y in x:
+#			var animPlayer = y.get_node("Roof").get_node("AnimationPlayer")
+#			if(animPlayer):
+#				animPlayer.current_animation = "FadeOut"
