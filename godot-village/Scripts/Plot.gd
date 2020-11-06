8db062e7d9fa5cfdad53dbf5146c4ec6e7c9c511
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-11-06 13:59:08 +0100

    added placing objects

--- godot-village/Scripts/Plot.gd
+++ godot-village/Scripts/Plot.gd
@@ -2,24 +2,29 @@ extends Area2D
 
 export var gridSize = 64
 export(Array, PackedScene) var blocks
+export(Array, PackedScene) var roofs
+export(Array, PackedScene) var objects
 
 var width = 8
 var height = 8
 var placedBlocks = []
 var placedRoofs = []
+var placedObjects = []
 
 var currentBlockType = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	placedBlocks = createType(placedBlocks)
+	placedRoofs = createType(placedRoofs)
+	placedObjects = createType(placedObjects)
+	
+func createType(type):
 	for x in range(width):
 		var col = []
 		col.resize(height)
-		placedBlocks.append(col)
-	for x in range(width):
-		var col = []
-		col.resize(height)
-		placedRoofs.append(col)
+		type.append(col)
+	return type
 
 
 func _on_Plot_input_event(viewport, event, shape_idx):
@@ -31,31 +36,39 @@ func _on_Plot_input_event(viewport, event, shape_idx):
 		gridPos.x = round(((get_global_mouse_position().x-position.x)-(gridSize/2))/gridSize)
 		gridPos.y = round(((get_global_mouse_position().y-position.y)-(gridSize/2))/gridSize)
 		
-		if(event.is_action_pressed("mouse_left") && gridPos.x >= 0 && gridPos.y >= 0 && gridPos.x < width && gridPos.y < width):
+		if(gridPos.x >= 0 && gridPos.y >= 0 && gridPos.x < width && gridPos.y < width):
 			
-			if(currentBlockType == 2):
-				placedRoofs = placeBlock(placedRoofs, gridPos)
+			if(currentBlockType < blocks.size()):
+				if(event.is_action_pressed("mouse_left")):
+					placedBlocks = placeBlock(placedBlocks, gridPos, blocks, currentBlockType)
+				elif(event.is_action_pressed("mouse_right")):
+					placedBlocks = removeBlock(placedBlocks, gridPos)
+			elif(currentBlockType < blocks.size() + roofs.size()):
+				if(event.is_action_pressed("mouse_left")):
+					placedRoofs = placeBlock(placedRoofs, gridPos, roofs, currentBlockType - blocks.size())
+				elif(event.is_action_pressed("mouse_right")):
+					placedRoofs = removeBlock(placedRoofs, gridPos)
 			else:
-				placedBlocks = placeBlock(placedBlocks, gridPos)
+				if(event.is_action_pressed("mouse_left")):
+					placedObjects = placeBlock(placedObjects, gridPos, objects, currentBlockType - blocks.size() - roofs.size())
+				elif(event.is_action_pressed("mouse_right")):
+					placedObjects = removeBlock(placedObjects, gridPos)
 				
-		elif(event.is_action_pressed("mouse_right")):
-			placedRoofs = removeBlock(placedRoofs, gridPos)
-			placedBlocks = removeBlock(placedBlocks, gridPos)
 
-func placeBlock(list, gridPos):
-	if(!list[gridPos.x][gridPos.y]):
+func placeBlock(placedBlocks, gridPos, blockList, blockIndex):
+	if(!placedBlocks[gridPos.x][gridPos.y]):
 		
-		var block = blocks[currentBlockType].instance()
+		var block = blockList[blockIndex].instance()
 		block.position = gridPos*gridSize+Vector2(32,32)
-		list[gridPos.x][gridPos.y] = block
+		placedBlocks[gridPos.x][gridPos.y] = block
 		add_child(block)
 		
-	return list
+	return placedBlocks
 
-func removeBlock(list, gridPos):
-	if(list[gridPos.x][gridPos.y]):
-		list[gridPos.x][gridPos.y].queue_free()
-	return list
+func removeBlock(placedBlocks, gridPos):
+	if(placedBlocks[gridPos.x][gridPos.y]):
+		placedBlocks[gridPos.x][gridPos.y].queue_free()
+	return placedBlocks
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -64,7 +77,7 @@ func _process(delta):
 		currentBlockType += 1
 	if(Input.is_action_just_pressed("blockPrev")):
 		currentBlockType -= 1
-	currentBlockType = clamp(currentBlockType, 0, blocks.size()-1)
+	currentBlockType = clamp(currentBlockType, 0, blocks.size()+roofs.size()+objects.size()-1)
 
 
 
